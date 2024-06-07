3a23edbe4e18dc66f5f64f80c36808384af1115e
alanysouza <2021345@student.cct.ie>
2024-06-07 11:36:42 +0100

    set the game
    Set how the game will work and movements possibilities
    Still need changes

--- scripts/main.js
+++ scripts/main.js
@@ -260,3 +260,472 @@ function fillGridSquaresArray() {
       currentCell.pieceType = "blank";
       currentCell.pieceId = "blank";
   }
+  setupGridSquares();
+setupPieces();
+fillGridSquaresArray();
+
+//Allows squares to receive pieces.
+function setupGridSquares() {
+  for (let i = 0; i < gridsquares.length; i++) {
+    gridsquares[i].addEventListener("dragover", allowDrop);
+    gridsquares[i].addEventListener("drop", drop);
+    let row = 11 - Math.floor(i / 12);
+    let column = String.fromCharCode(96 + (i % 12));
+    let square = gridsquares[i];
+    square.id = column + row;
+  }
+}
+
+// Allows pieces (monsters) to be moved.
+function setupPieces() {
+  for (let i = 0; i < pieces.length; i++) {
+    pieces[i].addEventListener("dragstart", drag);
+    pieces[i].setAttribute("draggable", true);
+    pieces[i].id = pieces[i].className.split(" ")[1] + pieces[i].parentElement.id;
+  }
+  for (let i = 0; i < piecesImages.length; i++) {
+    piecesImages[i].setAttribute("draggable", false);
+  }
+}
+
+//preventents non intentional moves 
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+//Controls when a piece is moved
+function drag(event) {
+  const piece = event.target;
+
+  const pieceColor = piece.getAttribute("color");
+  const pieceType =piece.classList[1];
+  const pieceId = piece.id;
+
+  // Checks if the piece is of the current player's color.
+  if (numToText(currentPlayerTurn) == pieceColor) {
+    const startingCellId = piece.parentNode.id;
+    event.dataTransfer.setData("text", pieceId + "|" + startingCellId);
+    const pieceObject ={pieceColor:pieceColor, pieceType:pieceType, pieceId:pieceId}
+
+    let legalMoves = getLegalMoves(
+      startingCellId,
+      pieceObject,
+      gridSquareArray
+    );
+
+    let legalMovesJson = JSON.stringify(legalMoves);
+    event.dataTransfer.setData("application/json", legalMovesJson);
+  }
+
+  //Game must be started for user to be allowed to play
+  else if (btnStart.textContent == "Start Game")
+    alert("Press Start Button to play");
+
+  //Selected piece is another colour
+  else
+    alert(`It's ${numToText(currentPlayerTurn)}'s turn!`);
+
+}
+
+//function to handle the move of a piece to a cell
+function drop(event) {
+  event.preventDefault();
+
+  //Extract information about the moved piece and where it is coming from.
+  let data = event.dataTransfer.getData("text");
+
+  let [pieceId, startingCellId] = data.split("|");
+  let legalMovesJson = event.dataTransfer.getData("application/json");
+  let legalMoves = JSON.parse(legalMovesJson);
+
+  const piece = document.getElementById(pieceId);
+  const pieceColor = piece.getAttribute("colour");
+  const pieceType = piece.classList[1];
+
+  const targetCell = event.currentTarget;
+  let   targetCellId = targetCell.id;
+
+  // Check if there is a piece in the target cell
+  let squareContent = getPieceAtSquare(targetCellId,gridSquareArray);
+
+  //Check if the movement is allowed
+  if(!legalMoves.includes(targetCellId)) {
+    alert(`Invalid move. \nValid moves for this monster: ${legalMoves}`);
+    return;
+  }
+
+  //Place the piece in the target cell
+  targetCell.appendChild(piece);
+
+  let children = targetCell.children;
+  let survivor = 0;
+
+  if (squareContent.pieceType != "blank") {
+    if(squareContent.pieceType == pieceType) {
+      alert(`${squareContent.pieceType} x ${pieceType}: both dead!`);
+
+      //Remove pieces from the cells 
+      for (let i = 0; i < children.length; i++) {
+        if (!children[i].classList.contains('coordinate')) {
+          targetCell.removeChild(children[i--]);
+        }
+      }
+
+      players[squareContent.pieceColor]["monsters"]--;
+      players[squareContent.pieceColor][squareContent.pieceType]--;
+      players[pieceColor]["monsters"]--;
+      players[pieceColor][pieceType]--;
+
+      survivor = -1;
+    }
+
+    //Different monsters
+    else {
+
+      targetCell.appendChild(piece);
+      let children = targetCell.children;
+
+      //Check the rules to see who survives 
+      if((squareContent.pieceType == "werewolf" && pieceType == "vampire") ||
+         (squareContent.pieceType == "vampire" && pieceType == "ghost") ||
+         (squareContent.pieceType == "ghost" && pieceType == "werewolf")){
+
+        alert(`${pieceType} x ${squareContent.pieceType}: ${squareContent.pieceType} dead!`);
+
+        //Remove the loser from the cell
+        targetCell.removeChild(children[0]);
+
+        players[squareContent.pieceColor]["monsters"]--;
+        players[squareContent.pieceColor][squareContent.pieceType]--;
+      }
+      else{
+        alert(`${pieceType} x ${squareContent.pieceType}: ${pieceType} dead!`);
+        targetCell.removeChild(children[1]);
+        players[pieceColor]["monsters"]--;
+        players[pieceColor][pieceType]--;
+        survivor = 1;
+      }
+    }
+  }
+
+  updateGridSquaresArray(startingCellId, targetCellId, gridSquareArray, survivor);
+
+  //Update the information
+  updatePlayerInfo();
+  return;
+}
+
+//Check if the move selected is legal
+function getLegalMoves(startingCellId, piece, gridSquareArray) {
+
+  let horiVert = getHoriVertMoves(startingCellId, piece.pieceColor, gridSquareArray);
+  let diagonal = getDiagonalMoves(startingCellId, piece.pieceColor, gridSquareArray);
+  let legalMoves = [...horiVert, ...diagonal];
+  return legalMoves;
+}
+//Horizontal and vertical moves
+function getHoriVertMoves(startingCellId, pieceColor, gridSquareArray) {
+    let vetMoveAllUp = moveAllUp(startingCellId, pieceColor, gridSquareArray);
+    let vetMoveAllDown = moveAllDown( startingCellId, pieceColor, gridSquareArray);
+    let vetMoveAllLeft = moveAllLeft( startingCellId, pieceColor, gridSquareArray);
+    let vetMoveAllRight = moveAllRight( startingCellId, pieceColor, gridSquareArray);
+    let legalMoves = [
+      ...vetMoveAllUp,
+      ...vetMoveAllDown,
+      ...vetMoveAllLeft,
+      ...vetMoveAllRight,
+    ];
+    return legalMoves;
+  }
+  
+  //check possibilities
+  function getDiagonalMoves(startingCellId, pieceColor, gridSquareArray) {
+    let vetMoveToDirNE = moveToDirNE(
+      startingCellId,
+      pieceColor,
+      gridSquareArray
+    );
+    let vetMoveToDirNW = moveToDirNW(
+      startingCellId,
+      pieceColor,
+      gridSquareArray
+    );
+    let vetMoveToDirSE = moveToDirSE(
+      startingCellId,
+      pieceColor,
+      gridSquareArray
+    );
+    let vetMoveToDirSW = moveToDirSW(
+      startingCellId,
+      pieceColor,
+      gridSquareArray
+    );
+    let legalMoves = [
+      ...vetMoveToDirNE,
+      ...vetMoveToDirNW,
+      ...vetMoveToDirSE,
+      ...vetMoveToDirSW,
+    ];
+    return legalMoves;
+  }
+  
+  //check possible moves 
+  function moveAllUp(startingCellId, pieceColor, gridSquareArray) {
+    const file = startingCellId.charAt(0);
+    const rank = startingCellId.substring(1);
+  
+    const rankNumber = parseInt(rank);
+    let currentRank = rankNumber;
+    let legalMoves = [];
+  
+    while (currentRank < 10) {
+      currentRank++;
+      let currentCellId = file + currentRank;
+      let currentCell = gridSquareArray.find(
+        (element) => element.squareId === currentCellId
+      );
+      let squareContent = currentCell.pieceColor;
+      if (squareContent != "blank" && squareContent == pieceColor)
+        return legalMoves;
+      legalMoves.push(currentCellId);
+      if (squareContent != "blank" && squareContent != pieceColor)
+        return legalMoves;
+    }
+  
+    return legalMoves;
+  }
+  
+  //Check possibilities downward
+  function moveAllDown(startingCellId, pieceColor, gridSquareArray) {
+    const file = startingCellId.charAt(0);
+    const rank = startingCellId.substring(1);
+    const rankNumber = parseInt(rank);
+    let currentRank = rankNumber;
+    let legalMoves = [];
+  
+    while (currentRank > 1) {
+      currentRank--;
+      let currentCellId = file + currentRank;
+      let currentCell = gridSquareArray.find(
+        (element) => element.squareId === currentCellId
+      );
+      let squareContent = currentCell.pieceColor;
+      if (squareContent != "blank" && squareContent == pieceColor)
+        return legalMoves;
+      legalMoves.push(currentCellId);
+      if (squareContent != "blank" && squareContent != pieceColor)
+        return legalMoves;
+    }
+  
+    return legalMoves;
+  }
+  
+  //Check all possibilities left
+  function moveAllLeft(startingCellId, pieceColor, gridSquareArray) {
+    const file = startingCellId.charAt(0);
+    const rank = startingCellId.substring(1);
+    let currentFile = file;
+    let legalMoves = [];
+  
+    if (rank == 0 || rank == 11 || currentFile == "`") return legalMoves;
+    while (currentFile != "a") {
+      currentFile = String.fromCharCode(currentFile.charCodeAt(0) - 1);
+      let currentCellId = currentFile + rank;
+      let currentCell = gridSquareArray.find(
+        (element) => element.squareId === currentCellId
+      );
+      let squareContent = currentCell.pieceColor;
+      if (squareContent != "blank" && squareContent == pieceColor)
+        return legalMoves;
+      legalMoves.push(currentCellId);
+      if (squareContent != "blank" && squareContent != pieceColor)
+        return legalMoves;
+    }
+  
+    return legalMoves;
+  }
+  
+  //check possibilities rightward
+  function moveAllRight(startingCellId, pieceColor, gridSquareArray) {
+    const file = startingCellId.charAt(0);
+    const rank = startingCellId.substring(1);
+    let currentFile = file;
+    let legalMoves = [];
+  
+    if (rank == 0 || rank == 11 || currentFile == "k") return legalMoves;
+    while (currentFile != "j") {
+      currentFile = String.fromCharCode(
+        currentFile.charCodeAt(0) + 1
+      );
+      let currentCellId = currentFile + rank;
+      let currentCell = gridSquareArray.find(
+        (element) => element.squareId === currentCellId
+      );
+      let squareContent = currentCell.pieceColor;
+      if (squareContent != "blank" && squareContent == pieceColor)
+        return legalMoves;
+      legalMoves.push(currentCellId);
+      if (squareContent != "blank" && squareContent != pieceColor)
+        return legalMoves;
+    }
+  
+    return legalMoves;
+  }
+  
+  //Check possibilities northwest
+  function moveToDirNW(
+    startingCellId,
+    pieceColor,
+    gridSquareArray
+  ) {
+    const file = startingCellId.charAt(0);
+    const rank = startingCellId.substring(1);
+    const rankNumber = parseInt(rank);
+    let currentFile = file;
+    let currentRank = rankNumber;
+    let legalMoves = [];
+  
+    if (file == "`" || rank == 11) return legalMoves;
+  
+    let moves = 2;
+    while (!(currentFile == "a" || currentRank == 8 || moves == 0)) {
+      currentFile = String.fromCharCode(
+        currentFile.charCodeAt(0) - 1
+      );
+      currentRank++;
+      let currentCellId = currentFile + currentRank;
+      let currentCell = gridSquareArray.find(
+        (element) => element.squareId === currentCellId
+      );
+      let squareContent = currentCell.pieceColor;
+      if (squareContent != "blank" && squareContent == pieceColor)
+        return legalMoves;
+      legalMoves.push(currentCellId);
+      if (squareContent != "blank" && squareContent != pieceColor)
+        return legalMoves;
+      moves--;
+    }
+    return legalMoves;
+  }
+  
+  //Check possibilities northeast
+  function moveToDirNE(
+    startingCellId,
+    pieceColor,
+    gridSquareArray
+  ) {
+    const file = startingCellId.charAt(0);
+    const rank = startingCellId.substring(1);
+    const rankNumber = parseInt(rank);
+    let currentFile = file;
+    let currentRank = rankNumber;
+    let legalMoves = [];
+  
+    if (file == "k" || rank == 11) return legalMoves;
+  
+    let moves = 2;
+    while (!(currentFile == "j" || currentRank == 10 || moves == 0)) {
+      currentFile = String.fromCharCode(
+        currentFile.charCodeAt(0) + 1
+      );
+      currentRank++;
+      let currentCellId = currentFile + currentRank;
+      let currentCell = gridSquareArray.find(
+        (element) => element.squareId === currentCellId
+      );
+      let squareContent = currentCell.pieceColor;
+      if (squareContent != "blank" && squareContent == pieceColor)
+        return legalMoves;
+      legalMoves.push(currentCellId);
+      if (squareContent != "blank" && squareContent != pieceColor)
+        return legalMoves;
+      moves--;
+    }
+    return legalMoves;
+  }
+  
+  //Check all possibilities southeast
+  function moveToDirSW(startingCellId, pieceColor, gridSquareArray) {
+    const file = startingCellId.charAt(0);
+    const rank = startingCellId.substring(1);
+    const rankNumber = parseInt(rank);
+    let currentFile = file;
+    let currentRank = rankNumber;
+    let legalMoves = [];
+  
+    if (file == "`" || rank == 0) return legalMoves;
+  
+    let moves = 2;
+    while (!(currentFile == "a" || currentRank == 1 || moves == 0)) {
+      currentFile = String.fromCharCode(
+        currentFile.charCodeAt(0) - 1
+      );
+      currentRank--;
+      let currentCellId = currentFile + currentRank;
+      let currentCell = gridSquareArray.find(
+        (element) => element.squareId === currentCellId
+      );
+      let squareContent = currentCell.pieceColor;
+      if (squareContent != "blank" && squareContent == pieceColor)
+        return legalMoves;
+      legalMoves.push(currentCellId);
+      if (squareContent != "blank" && squareContent != pieceColor)
+        return legalMoves;
+      moves--;
+    }
+    return legalMoves;
+  }
+  
+  //check all the possibilities diagonal right.
+  function moveToDirSE(startingCellId, pieceColor, gridSquareArray) {
+    const file = startingCellId.charAt(0);
+    const rank = startingCellId.substring(1);
+    const rankNumber = parseInt(rank);
+    let currentFile = file;
+    let currentRank = rankNumber;
+    let legalMoves = [];
+  
+    if (file == "k" || rank == 0) return legalMoves;
+  
+    let moves = 2;
+    while (!(currentFile == "j" || currentRank == 1 || moves == 0)) {
+      currentFile = String.fromCharCode(
+        currentFile.charCodeAt(0) + 1
+      );
+      currentRank--;
+      let currentCellId = currentFile + currentRank;
+      let currentCell = gridSquareArray.find(
+        (element) => element.squareId === currentCellId
+      );
+      let squareContent = currentCell.pieceColor;
+      if (squareContent != "blank" && squareContent == pieceColor)
+        return legalMoves;
+      legalMoves.push(currentCellId);
+      if (squareContent != "blank" && squareContent != pieceColor)
+        return legalMoves;
+      moves--;
+    }
+    return legalMoves;
+  }
+  
+  //Chack if there is already a piece on the destination grid
+  function getPieceAtSquare(squareId, gridSquareArray) {
+    let currentCell = gridSquareArray.find(
+      (element) => element.squareId === squareId
+    );
+    const color = currentCell.pieceColor;
+    const pieceType = currentCell.pieceType;
+    const pieceId=currentCell.pieceId;
+    return { pieceColor: color, pieceType: pieceType,pieceId:pieceId};
+  }
+  
+  // Convert numbers to text
+  function numToText(i) {
+    switch(i){
+        case 0:   return "purple";
+        case 1:   return "red";
+        case 2:   return "pink";
+        case 3:   return "orange";
+        default:  return "invalid";
+    }
+  }
